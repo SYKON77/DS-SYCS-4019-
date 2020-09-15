6b5650ce7e66a445d8d1d1cf64c53bd6a544d035
SYKON77 <58018484+SYKON77@users.noreply.github.com>
2020-09-15 20:39:13 +0530

    Update Practical 1A.py

--- Practical 1A.py
+++ Practical 1A.py
@@ -1,11 +1,11 @@
-def searching(n, lst):
-    size_of_list = len(lst)
-    print("searching {} in list {}".format(n, lst))
-    if n not in lst:
+def searching(n, list1):
+    size_of_list = len(list1)
+    print("searching {} in list {}".format(n, list1))
+    if n not in list1:
         print(n, "is not in the list")
     else:
         for i in range(size_of_list):
-            if lst[i] == n:
+            if list1[i] == n:
                 print("index of ", n, " is ", i)
 
 
@@ -30,43 +30,43 @@ def InsertionSort(array):
         return array
 
 
-def merging(lst1, lst2):
-    print("merging..{} and {}".format(lst1, lst2))
-    list2 = [6, 3, 45, 23, 56, 67, 77, 65]
-    print("list1 = ", lst1)
-    print("list2 = ", lst2)
-    merge = lst1+lst2
+def merging(list11, list12):
+    print("merging..{} and {}".format(list11, list12))
+    lst2 = [6, 3, 45, 23, 56, 67, 77, 65]
+    print("list1 = ", list11)
+    print("list2 = ", list12)
+    merge = list11+list12
     return merge
 
 
-def revrse(lst):
-    print("reversig {}".format(lst))
-    size = len(lst)-1
+def revrse(list1):
+    print("reversig {}".format(list1))
+    size = len(list1)-1
     i = 0
     while size > 0:
-        lst[i], lst[size] = lst[size], lst[i]
+        list1[i], list1[size] = list1[size], list1[i]
         size -= 1
         i += 1
         if size <= i:
             break
-    return lst
+    return list1
 
 
 def main():
-    lst1 = [23, 45, 32, 34, 55, 66, 2, 33, 12, 3, 4]
-    lst2 = [23, 42, 12, 34, 33, 22, 11, 44, 33, 45, 67]
-    print("given main list {}".format(lst1))
+    list11 = [23, 45, 32, 34, 55, 66, 2, 33, 12, 3, 4]
+    list12 = [23, 42, 12, 34, 33, 22, 11, 44, 33, 45, 67]
+    print("given main list {}".format(list11))
     element_to_search = int(input("enter the number to search: "))
-    searching(element_to_search, lst1)
+    searching(element_to_search, list11)
     print("\n")
-    print("original list {}".format(lst1))
-    sorted_list = bubbleSort(lst1)
+    print("original list {}".format(list11))
+    sorted_list = bubbleSort(list11)
     print("sorted list {}".format(sorted_list))
     print("\n")
-    merged_list = merging(lst1, lst2)
+    merged_list = merging(list11, list12)
     print("mergelist {}".format(merged_list))
     print("\n")
-    reversed_list = revrse(lst2)
+    reversed_list = revrse(list12)
     print("reversed list {}".format(reversed_list))
 
 
