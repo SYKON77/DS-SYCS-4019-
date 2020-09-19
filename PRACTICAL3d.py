8702e341f334a4e15a84d99030ccdf82235ea4be
SYKON77 <58018484+SYKON77@users.noreply.github.com>
2020-09-19 21:09:20 +0530

    Update PRACTICAL3d.py

--- PRACTICAL3d.py
+++ PRACTICAL3d.py
@@ -1,3 +1,4 @@
+#PRACTICAL3d (1)
 def factor(num):
     if num < 0:
         print('Factorial does not exist')
@@ -10,3 +11,18 @@ def factor(num):
             return num * factor(num-1) 
 
 print(factor(6))
+
+#PRACTICAL3d (2)
+
+def factor(num):
+    factorial = 1
+    if num < 0:
+        print("factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        for i in range(1,num + 1):
+           factorial = factorial*i
+        print("The factorial is: ",factorial)
+
+factor(88)
